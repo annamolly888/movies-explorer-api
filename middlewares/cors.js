80606a775643667cbe8b515224a67f8fe6c58446
alenamakarovaya <annamolly888@yandex.ru>
2024-03-13 00:49:01 +0300

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,28 +1,28 @@
 const allowedCors = [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'http://movies.annamolly888.nomoredomainswork.ru',
-    'https://movies.annamolly888.nomoredomainswork.ru',
-  ];
-  
-  const cors = (req, res, next) => {
-    const { origin } = req.headers;
-    const { method } = req;
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-  
-    const requestHeaders = req.headers['access-control-request-headers'];
-  
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
-  
-    return next();
-  };
-  
-  module.exports = { cors };
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://movies.annamolly888.nomoredomainswork.ru',
+  'https://movies.annamolly888.nomoredomainswork.ru',
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Expose-Headers', 'Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    return res.end();
+  }
+
+  return next();
+};
+
+module.exports = { cors };
