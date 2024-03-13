ce2dd3619c667b643467c341ff05cd569273a841
alenamakarovaya <annamolly888@yandex.ru>
2024-03-13 10:23:03 +0300

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,19 +7,20 @@ const allowedCors = [
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
-
+  const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', requestHeaders || '');
 
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Expose-Headers', 'Authorization');
-    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
-    return res.end();
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Max-Age', 3600);
+      return res.status(200).send();
+    }
   }
 
   return next();
